101d8ffcfeeb9c8a28eccdd9a6de846af24dcb2d
james <jtwebco@gmail.com>
2020-11-29 13:15:32 -0500

    adds discord link

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { InstagramIcon, Twitter, Youtube, Facebook } from '../icons/icons'
+import { InstagramIcon, Twitter, Discord, Youtube, Facebook } from '../icons/icons'
 
 const Socials = ({ className }) => {
     return (
         <ul className='socials animate--fade-in'>
             <li><a title='nrdstr on instagram' href='https://www.instagram.com/nrdstr_/'><InstagramIcon /></a></li>
+            <li><a title='nrdstr community on discord' href='https://discord.gg/7tzT2R4F4M'><Discord /></a></li>
             <li><a title='nrdstr on twitter' href='https://twitter.com/nrdstr_'><Twitter /></a></li>
             <li><a title='nrdstr on facebook' href='https://www.facebook.com/nrdstr.design'><Facebook /></a></li>
             <li><a title='nrdstr on youtube' href='https://www.youtube.com/channel/UC-vQ0EMVGGOcSIw781MYOTg'><Youtube /></a></li>
