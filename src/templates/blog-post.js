42cfd6bc5356152d5f5b545089d190e3af17083d
james <jtwebco@gmail.com>
2020-12-13 13:06:01 -0500

    tweak

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -58,7 +58,7 @@ export default ({ data }) => {
     body.scrollTo(0, 0)
     if (content.current.children) {
       const video = document.querySelector('.wp-block-embed__wrapper')
-      console.log(video)
+      // console.log(video)
       if (video.children) video.children[0].classList.add('blog__video-iframe')
 
       const links = content.current.querySelectorAll('#link')
